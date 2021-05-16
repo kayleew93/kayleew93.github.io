044f2ae01520befc66771ba90408f0f605d0992c
kayleew93 <kayleew93@gmail.com>
2021-05-15 21:29:27 -0600

    preston id page updates

--- lesson4/js/preston-4.js
+++ lesson4/js/preston-4.js
@@ -1,3 +1,17 @@
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("hide");
-}
+
+/* document.getElementById("primaryNav").classList.toggle("hide"); */
+const navbutton = document.querySelector('.hambutton');
+const mainnav = document.querySelector('.primaryNav');
+
+navbutton.addEventListener('click', () =>
+{mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760)
+mainnav.classList.remove('hide')};
+
+/* Current Date */
+const datefield = document.querySelector("date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle: "full" }).format(now);
+datefield.textContent = fulldate;
