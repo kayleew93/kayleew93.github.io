6c1c8437479ed226623df87cf522c818fa5a911f
kayleew93 <kayleew93@gmail.com>
2021-06-05 20:42:07 -0600

    added to code

--- lesson7/js/lastvisit.js
+++ lesson7/js/lastvisit.js
@@ -1,31 +1,22 @@
 /* ----- Get how many days since last visit ----- */
 
-/*
-function onVisited(historyItem) {
-    console.log(historyItem.url);
-    console.log(new Date(historyItem.lastVisitTime));
-  }
-
-window.History.onVisited.addListener(onVisited);
-*/
-
 
 window.onload = function() {
 
     // function to calculate days between visits
     function daysBetween(start, end) {
 
-        const oneDay = 1000; // * 60 * 60 * 24
+        const oneDay = 1000 * 60 * 60 * 24;
 
         const diffInTime = start - end;
 
-        const diffInDays = Math.round(diffInTime / oneDay); // -1
+        const diffInDays = Math.round(diffInTime / oneDay) - 1;
 
         return diffInDays
     }
 
     // Check browser support
-    if (typeof(Storage) !== "undefined") {
+    if (typeof(Storage) != "undefined") {
 
         let newVisitDate = new Date();
 
@@ -47,7 +38,7 @@ window.onload = function() {
 
         } else {
 
-        lastVisit = newVisit;
+        let lastVisit = newVisit;
 
         // Store recent visit
         localStorage.setItem("recentVisit", lastVisit);
@@ -57,6 +48,7 @@ window.onload = function() {
         // DELETE LATER -- CHECKS TO MAKE SURE VALUE WAS REASSIGNED
         console.log(lastVisit);
         console.log(localStorage.getItem("recentVisit"));
+        console.log(typeof lastVisit);
 
     }} else {
         document.querySelector(".lastvisit").innerText = "Sorry, your browser does not support Web Storage...";
