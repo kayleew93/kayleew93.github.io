a5fbf8fb803e54e075543d823134e7ba7cd3d4ab
kayleew93 <kayleew93@gmail.com>
2021-06-22 17:29:21 -0600

    lesson10 initial upload

---
+++ lesson10/js/weatherapi10.js
@@ -0,0 +1,16 @@
+
+//const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=524901&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962";
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    document.querySelector("#tempF").textContent = jsObject.main.temp;
+/*
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description
+    document.querySelector("#imagesrc").textContent = imagesrc;
+    document.querySelector("#icon").setAttribute("src", imagesrc);
+    document.querySelector("#icon").setAttribute("alt", desc); */
+  });
---
+++ lesson10/js/windchill.js
@@ -0,0 +1,60 @@
+/* ------ CALCULATE WINDCHILL ------ 
+let tempF = document.querySelector(".tempF").innerHTML;
+let windSpeed = document.querySelector(".windspeed").innerHTML;
+
+function windChill(tempF, windSpeed) {
+  let output =
+    35.74 +
+    0.6215 * tempF -
+    35.75 * Math.pow(windSpeed, 0.16) +
+    0.4275 * tempF * Math.pow(windSpeed, 0.16);
+
+  document.querySelector("#windchill").innerHTML =
+    output.toFixed(1) + "&#176;F";
+}
+
+let wind_chill =
+  tempF <= 50 && windSpeed >= 3
+    ? windChill(tempF, windSpeed)
+    : (document.querySelector("#windchill").innerHTML = "N/A");
+
+  */
+
+/*
+let tempF = parseFloat(document.querySelector(".tempF").innerText);
+let windSpeed = parseFloat(document.querySelector(".windspeed").innerText);
+
+function windChill(tempF, windSpeed) {
+  let output =
+    35.74 +
+    0.6215 * tempF -
+    35.75 * Math.pow(windSpeed, 0.16) +
+    0.4275 * tempF * Math.pow(windSpeed, 0.16);
+
+  document.querySelector("#windchill").innerHTML =
+    output.toFixed(1) + "&#176;F";
+}
+
+let wind_chill =
+  tempF <= 50 && windSpeed >= 3
+    ? windChill(tempF, windSpeed)
+    : (document.querySelector("#windchill").innerHTML = "N/A");
+*/
+
+let tempF = document.querySelector("#tempF");
+let windSpeed = parseFloat(document.querySelector("#windspeed").innerText);
+
+console.log(tempF);
+
+function windChill(tempF, windSpeed) {
+  return 35.74 +
+    0.6215 * tempF -
+    35.75 * Math.pow(windSpeed, 0.16) +
+    0.4275 * tempF * Math.pow(windSpeed, 0.16);
+}
+
+let wind_chill =
+  tempF <= 50 && windSpeed >= 3
+    ? windChill(tempF, windSpeed).toFixed(1) : "N/A";
+
+    document.querySelector("#windchill").innerText = wind_chill;
