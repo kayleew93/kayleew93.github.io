49b2d6f41751850bb3412e4cfec72d2c914c162d
kayleew93 <kayleew93@gmail.com>
2021-06-24 21:55:39 -0600

    lessno 10 updates

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,12 +1,12 @@
 
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962"
 
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.querySelector("#current-temp").textContent = jsObject.main.temp;
+    document.querySelector("#current-temp").textContent = jsObject.main.temp.toFixed(0);
 
     const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description
--- lesson10/js/weatherapi10.js
+++ lesson10/js/weatherapi10.js
@@ -1,16 +1,65 @@
+function windChill(tempF, windSpeed) {
+    return 35.74 +
+      0.6215 * tempF -
+      35.75 * Math.pow(windSpeed, 0.16) +
+      0.4275 * tempF * Math.pow(windSpeed, 0.16);
+  }
 
-//const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=524901&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962";
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962";
+// Information for current weather
+const apiURLcurrent =
+  "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962";
+fetch(apiURLcurrent)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.querySelector("#weathertype").textContent =
+      jsObject.weather[0].description;
+    document.querySelector("#tempF").textContent =
+      jsObject.main.temp.toFixed(0);
+    document.querySelector("#humidity").textContent = jsObject.main.humidity;
+    document.querySelector("#windspeed").textContent = jsObject.wind.speed;
 
+    // if parameters are met, wind chill will be called
+    if (jsObject.main.temp <= 50 && jsObject.wind.speed >= 3) {
+        chill = windChill(jsObject.main.temp, jsObject.wind.speed);
+        document.querySelector("#windchill").textContent = chill;
+    }
+    else {
+        document.querySelector("#windchill").textContent = 'N/A';
+    };
+  });
+
+  // Information for forecast
+const apiURL =
+  "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    document.querySelector("#tempF").textContent = jsObject.main.temp;
-/*
-    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-    const desc = jsObject.weather[0].description
-    document.querySelector("#imagesrc").textContent = imagesrc;
-    document.querySelector("#icon").setAttribute("src", imagesrc);
-    document.querySelector("#icon").setAttribute("alt", desc); */
+    console.log(jsObject);
+    let day = 1;
+    const dayofWeek = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+
+    const fiveDayForecast = jsObject.list.filter((forecast) =>
+      forecast.dt_txt.includes("18:00:00")
+    );
+    console.log(fiveDayForecast);
+
+    fiveDayForecast.forEach((x) => {
+      let d = new Date(x.dt_txt);
+      document.querySelector(`#dayofweek${day}`).textContent =
+        dayofWeek[d.getDay()];
+      document.querySelector(`#dailytemp${day}`).textContent =
+        x.main.temp.toFixed(0);
+
+      const imagesrc = `https://openweathermap.org/img/w/${x.weather[0].icon}.png`;
+      const desc = x.weather[0].description;
+      document.querySelector(`#icon${day}`).setAttribute("src", imagesrc);
+      document.querySelector(`#icon${day}`).setAttribute("alt", desc);
+
+      day++;
+    });
+
+    /*
+     */
   });
