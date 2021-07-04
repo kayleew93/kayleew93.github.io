cb09f865ad5d87069471d867978e396ea23e71fc
kayleew93 <kayleew93@gmail.com>
2021-07-03 20:03:01 -0600

    preston final

--- lesson11/js/preston.js
+++ lesson11/js/preston.js
@@ -1,27 +1,3 @@
-const navbutton = document.querySelector(".hambutton");
-const mainnav = document.querySelector(".primaryNav");
-
-navbutton.addEventListener(
-  "click",
-  () => {
-    mainnav.classList.toggle("responsive");
-  },
-  false
-);
-
-window.onresize = () => {
-  if (window.innerWidth > 760) mainnav.classList.remove("hide");
-};
-
-/* Current Date */
-const datefield = document.querySelector("date");
-
-const now = new Date();
-
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-  now
-);
-datefield.textContent = fulldate;
 
 let pancakebanner =
   now.getDay() == 5
