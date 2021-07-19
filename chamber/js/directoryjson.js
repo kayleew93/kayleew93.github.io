255192e7c1ecbff03f6faac2f94f880aab3a43d1
kayleew93 <kayleew93@gmail.com>
2021-07-19 11:08:56 -0600

    json updates

--- chamber/js/directoryjson.js
+++ chamber/js/directoryjson.js
@@ -1,16 +1,10 @@
-/*
-const directory = "../json/directory.json"
-
-fetch(directory)
-  .then(function (response) {
-    return response.json();
-  })
+fetch("/chamber/json/directory.json")
+  .then((response) => response.json())
   .then(function (jsonObject) {
-    const businesses = jsonObject["businesses"];*/
+    const businesses = jsonObject["businesses"];
 
-const businesses = JSON.parse("../json/directory.json");
-
-    businesses.forEach(business => {
+    businesses.forEach((business) => {
+        
         let card = document.createElement("section");
         let name = document.createElement("h2");
         let phone = document.createElement("p");
@@ -22,20 +16,15 @@ const businesses = JSON.parse("../json/directory.json");
         phone.textContent = `${business.phone}`;
         address.textContent = `${business.address}`;
         link.textContent = `${business.link}`;
-        logo.setAttribute("src", business.logo);
-        logo.setAttribute(
-            "alt",
-            `${business.name} logo`
-        );
-        
+        logo.setAttribute("src", `images/${business.logo}`);
+        logo.setAttribute("alt", `${business.name} logo`);
+
         card.appendChild(logo);
         card.appendChild(name);
         card.appendChild(phone);
         card.appendChild(address);
         card.appendChild(link);
-        
-    
+
         document.querySelector("div.buscards").appendChild(card);
-      })
-      /*
-    });*/
+      });
+    });
