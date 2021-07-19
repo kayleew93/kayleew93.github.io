93bf6e802e572aab3394897f3dd630ef04070280
kayleew93 <kayleew93@gmail.com>
2021-07-19 16:26:19 -0600

    added upcoming events

---
+++ chamber/js/upcomingevents.js
@@ -0,0 +1,28 @@
+fetch("/chamber/json/upcomingevents.json")
+  .then((response) => response.json())
+  .then(function (jsonObject) {
+    const events = jsonObject["events"];
+
+    events.forEach((event) => {
+      let card = document.createElement("li");
+      let name = document.createElement("h3");
+      let date = document.createElement("p");
+      let description = document.createElement("p");
+      let link = document.createElement("a");
+      link.setAttribute("href", `${event.link}`);
+      link.setAttribute("target", "_blank");
+      link.innerText = "More info";
+      
+      name.textContent = `${event.name}`;
+      date.textContent = `${event.date}`;
+      description.textContent = `${event.description}`;
+      
+      card.appendChild(name);
+      card.appendChild(date);
+      card.appendChild(description);
+      card.appendChild(link);
+
+      document.querySelector(".upcoming_events").appendChild(card);
+
+    });
+  });
--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -1,33 +1,53 @@
-lat = 41.0602
-long = -111.971
-apiKey = '9cef91c93e68d2c9661f7465a0530962';
+lat = 41.0602;
+long = -111.971;
+apiKey = "9cef91c93e68d2c9661f7465a0530962";
 const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
 
 fetch(apiURL)
-.then((response) => response.json())
-.then((jsObject) => {
-    console.log(jsObject);
-
+  .then((response) => response.json())
+  .then((jsObject) => {
     document.querySelector("#weatherDesc").textContent =
-    jsObject.current.weather[0].description;
+      jsObject.current.weather[0].description;
     document.querySelector("#tempF").textContent =
-    jsObject.current.temp.toFixed(0);
+      jsObject.current.temp.toFixed(0);
     document.querySelector("#humidity").textContent = jsObject.current.humidity;
-    
 
-// Information for forecast
+    // Information for forecast
     let day = 1;
     const dayofWeek = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-    
-    const threeDayForecast = [jsObject.daily[1], jsObject.daily[2], jsObject.daily[3]];
-    
+
+    const threeDayForecast = [
+      jsObject.daily[1],
+      jsObject.daily[2],
+      jsObject.daily[3],
+    ];
+
     threeDayForecast.forEach((x) => {
-        let d = new Date(x.dt * 1000);
-    document.querySelector(`#dayofweek${day}`).textContent =
+      let d = new Date(x.dt * 1000);
+      document.querySelector(`#dayofweek${day}`).textContent =
         dayofWeek[d.getDay()];
-    document.querySelector(`#temp${day}`).textContent =
-        x.temp.day;    
-    day++;
+      document.querySelector(`#temp${day}`).textContent = x.temp.day;
+      day++;
     });
-});
-  
+
+    // Information for weather alert
+    if (jsObject.current.temp) {
+      document.querySelector("#weatherAlert").textContent =
+      "Hello, this is a test";
+      //jsObject.alerts.description;
+/*
+      const xbutton = document.querySelector(".xbutton");
+
+      navbutton.addEventListener(
+        "click",
+        () => {
+          mainnav.classList.toggle(".xbutton");
+        },
+        false
+      );
+
+      window.onresize = () => {
+        if (window.innerWidth > 760) mainnav.classList.remove("hide");
+      };*/
+    }
+  });
