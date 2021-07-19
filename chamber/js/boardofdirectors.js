93ef55ec769fbdc727e217697ec3dddcd21ade8d
kayleew93 <kayleew93@gmail.com>
2021-07-19 17:41:54 -0600

    board of directors

---
+++ chamber/js/boardofdirectors.js
@@ -0,0 +1,25 @@
+fetch("/chamber/json/boardofdirectors.json")
+  .then((response) => response.json())
+  .then(function (jsonObject) {
+    const boardmembers = jsonObject["boardmembers"];
+
+    boardmembers.forEach((member) => {
+      let card = document.createElement("section");
+      let name = document.createElement("p");
+      let position = document.createElement("h3");
+      let headshot = document.createElement("img");
+
+      headshot.setAttribute("src", `images/${member.photo}`);
+      headshot.setAttribute("alt", `${member.name}'s photo`);
+      
+      name.textContent = `${member.name}`;
+      position.textContent = `${member.position}`;
+      
+      card.appendChild(headshot);
+      card.appendChild(position);
+      card.appendChild(name);
+
+      document.querySelector(".boardofdirectors").appendChild(card);
+
+    });
+  });
